9618db3c8104d63b3a0193d96d77d0070add00b2
FreeForParrots <artemsupergen@gmail.com>
2023-02-04 14:56:05 +0200

    ShopM4

---
+++ ShopM4/Controllers/MyModelController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using ShopM4.Data;
+using ShopM4_Models;
+using ShopM4_Utility;
+
+namespace ShopM4.Controllers
+{
+    [Authorize(Roles = PathManager.AdminRole)]
+    public class MyModelController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public MyModelController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<MyModel> models = db.MyModel;
+
+            return View(models);
+        }
+    }
+}
+
